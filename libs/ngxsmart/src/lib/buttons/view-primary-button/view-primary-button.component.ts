45dbe6af3699b3261a0ff08ede43869e0ccd3ab7
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-04-14 00:12:06 -0400

    feat(buttons): adds bs-link and view-primary buttons

---
+++ libs/ngxsmart/src/lib/buttons/view-primary-button/view-primary-button.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, NgModule, Input } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
+@Component({
+	selector: 'view-primary-button',
+	template: `
+		<button class="primary-button" mat-raised-button type="{{ type }}"  data-cy="view-button">
+			<mat-icon>{{ icon }}</mat-icon>
+			{{ label }}
+		</button>
+	`,
+	styles: [],
+})
+export class ViewPrimaryButtonComponent {
+	/**
+	 * Type of the button. Following values are supported. See BootStrap docs for more information
+	 * <pre>
+	 *   1. button
+	 *   2. submit
+	 * </pre>
+	 */
+	@Input() type = 'button';
+
+	/**
+	 * If set, shows material icon
+	 */
+	@Input() icon = 'visibility';
+
+	/**
+	 * If set, shows when search is not in progress
+	 */
+	@Input() label = 'View';
+}
+
+@NgModule({
+	imports: [CommonModule, MatButtonModule, MatIconModule],
+	declarations: [ViewPrimaryButtonComponent],
+	exports: [ViewPrimaryButtonComponent],
+})
+export class ViewPrimaryButtonComponentModule {}
