f808a6017fe7d9ad29befd1e99503fb08c6dde02
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-05-02 22:26:00 -0400

    chore(docs): updates demo component

--- libs/ngxsmart/src/lib/buttons/edit-bs-button/edit-bs-button.component.ts
+++ libs/ngxsmart/src/lib/buttons/edit-bs-button/edit-bs-button.component.ts
@@ -1,46 +1,39 @@
-import { Component, Input, NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { EditSolidSvgComponentModule } from "../../svg-icons/edit-solid-svg/edit-solid-svg.component";
+import { Component, Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { EditSolidSvgComponentModule } from '../../svg-icons/edit-solid-svg/edit-solid-svg.component';
 
 @Component({
-  selector: "edit-bs-button",
-  template: `
-    <button
-      class="text-primary"
-      mat-button
-      color="primary"
-      data-cy="edit-bs-button"
-      type="{{type}}"
-    >
-      <edit-solid-svg></edit-solid-svg>
-      Edit
-    </button>
-  `,
-  styles: []
+	selector: 'edit-bs-button',
+	template: `
+		<button class="text-primary" mat-button color="primary" data-cy="edit-bs-button" type="{{ type }}">
+			<edit-solid-svg></edit-solid-svg>
+			{{ label }}
+		</button>
+	`,
+	styles: [],
 })
 export class EditBsButtonComponent {
-  /**
-   * Type of the button. Following values are supported. See BootStrap docs for mor
-   * e information
-   * <pre>
-   *   1. button
-   *   2. submit
-   * </pre>
-   */
-  @Input() type = "button";
+	/**
+	 * Type of the button. Following values are supported. See BootStrap docs for mor
+	 * e information
+	 * <pre>
+	 *   1. button
+	 *   2. submit
+	 * </pre>
+	 */
+	@Input() type = 'button';
 
-  /**
-   * If set, shows the label
-   */
-  @Input() label = "Edit";
+	/**
+	 * If set, shows the label
+	 */
+	@Input() label = 'Edit';
 }
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatIconModule, EditSolidSvgComponentModule],
-  declarations: [EditBsButtonComponent],
-  exports: [EditBsButtonComponent]
+	imports: [CommonModule, MatButtonModule, MatIconModule, EditSolidSvgComponentModule],
+	declarations: [EditBsButtonComponent],
+	exports: [EditBsButtonComponent],
 })
-export class EditBsButtonComponentModule {
-}
+export class EditBsButtonComponentModule {}
