1e2cf903056e2146918a21fe792311fbe8333517
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-08-25 21:28:25 -0400

    build(npm): upgrades NX to `14.5.10`

--- apps/ngxsmart-demo/jest.config.ts
+++ apps/ngxsmart-demo/jest.config.ts
@@ -1,5 +1,6 @@
+/* eslint-disable */
 module.exports = {
-	displayName: 'ngxsmart',
+	displayName: 'ngxsmart-demo',
 	preset: '../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 	globals: {
@@ -8,7 +9,7 @@ module.exports = {
 			stringifyContentPathRegex: '\\.(html|svg)$',
 		},
 	},
-	coverageDirectory: '../../coverage/libs/ngxsmart',
+	coverageDirectory: '../../coverage/apps/ngxsmart-demo',
 	transform: {
 		'^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
 	},
--- libs/ngxsmart/jest.config.ts
+++ libs/ngxsmart/jest.config.ts
@@ -1,5 +1,6 @@
+/* eslint-disable */
 module.exports = {
-	displayName: 'ngxsmart-demo',
+	displayName: 'ngxsmart',
 	preset: '../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 	globals: {
@@ -8,7 +9,7 @@ module.exports = {
 			stringifyContentPathRegex: '\\.(html|svg)$',
 		},
 	},
-	coverageDirectory: '../../coverage/apps/ngxsmart-demo',
+	coverageDirectory: '../../coverage/libs/ngxsmart',
 	transform: {
 		'^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
 	},
