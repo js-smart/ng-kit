161e2daab4cfdcfcdd58831d21d7208e1efcfe92
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-12-24 13:19:03 -0500

    feat(core): migrates angular modules to standalone components

--- apps/ngxsmart-demo/src/app/app.component.ts
+++ apps/ngxsmart-demo/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
 	standalone: true,
 	selector: 'ngxsmart-root',
 	templateUrl: './app.component.html',
+	imports: [RouterOutlet, RouterLink],
 })
 export class AppComponent {}
