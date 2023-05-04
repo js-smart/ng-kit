b0828dfc666f106495df8b86da5c57b0b16e0fd5
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-05-04 16:00:34 -0400

    feat(core): upgrades angular deps to 16.0.0

--- apps/ngxsmart-demo/src/main.ts
+++ apps/ngxsmart-demo/src/main.ts
@@ -1,50 +1,11 @@
-import { CommonModule, DatePipe } from '@angular/common';
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
-import { MatSelectModule } from '@angular/material/select';
-import { bootstrapApplication, BrowserModule, Title } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PreloadAllModules, RouterModule } from '@angular/router';
+import { appConfig } from './app/app.config';
+
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
 	enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-	providers: [
-		importProvidersFrom(
-			BrowserModule,
-			CommonModule,
-			RouterModule.forRoot(routes, {
-				anchorScrolling: 'enabled',
-				onSameUrlNavigation: 'reload',
-				scrollPositionRestoration: 'enabled',
-				preloadingStrategy: PreloadAllModules,
-				enableTracing: false,
-			}),
-			FormsModule,
-			ReactiveFormsModule,
-			BrowserAnimationsModule,
-			HttpClientModule,
-			HttpClientXsrfModule.withOptions(),
-			MatSelectModule,
-			MatAutocompleteModule,
-			MatNativeDateModule,
-			MatDatepickerModule
-		),
-		Title,
-		DatePipe,
-		{
-			provide: MAT_DIALOG_DEFAULT_OPTIONS,
-			useValue: { hasBackdrop: true, disableClose: true },
-		},
-	],
-}).catch((err) => console.error('Unable to Boostrap the application. Error:' + err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error('Unable to Boostrap the application. Error:' + err));
