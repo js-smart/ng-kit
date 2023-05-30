94716775aee7fa44727dd25176ea1af6336b728e
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-05-30 18:05:42 -0400

    feat(core): removes cypress and adds playwright

--- apps/ngxsmart-demo/src/main.ts
+++ apps/ngxsmart-demo/src/main.ts
@@ -1,11 +1,5 @@
 import { appConfig } from './app/app.config';
-
 import { AppComponent } from './app/app.component';
-
-import { environment } from './environments/environment';
-
-if (environment.production) {
-	enableProdMode();
-}
+import { bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error('Unable to Boostrap the application. Error:' + err));
