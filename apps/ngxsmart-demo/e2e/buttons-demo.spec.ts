cf877251696fc517b8cfeb4aa2db8889974d0f3f
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-09-18 22:31:48 -0400

    Improve e2e testing for buttons-demo
    Enhanced the end-to-end testing site by adding tests for button visibility in the demo application. The 'data-cy' attribute was also added to buttons to improve their identification during tests. These changes enhance the reliability of interaction in e2e testing.

--- apps/ngxsmart-demo/e2e/buttons-demo.spec.ts
+++ apps/ngxsmart-demo/e2e/buttons-demo.spec.ts
@@ -16,3 +16,12 @@ test('has all buttons', async ({ page }) => {
 	await expect(page.locator('[data-cy=pdf-export-button]')).toBeVisible();
 	await expect(page.locator('[data-cy=excel-export-button]')).toBeVisible();
 });
+
+test('has progress states', async ({ page }) => {
+	await page.goto('/buttons-demo');
+
+	await page.locator('[data-cy=success-button]').click();
+	await expect(page.getByText('Updating...')).toBeVisible();
+	await expect(page.getByText('Deleting...')).toBeVisible();
+	await expect(page.getByText('Saving...')).toHaveCount(2);
+});
