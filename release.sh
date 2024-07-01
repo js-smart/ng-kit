6755d8cdadee53f6ff4148c0e9f202a51a5547df
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-07-01 16:33:25 -0400

    fix(release): fixes issue with release

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the first argument
+next_version=$1
+
+# Path to the package.json file
+PACKAGE_JSON="projects/ng-kit/package.json"
+
+# Update the version in package.json
+jq --arg v "$next_version" '.version = $v' "$PACKAGE_JSON" > "tmp.json" && mv "tmp.json" "$PACKAGE_JSON"
+
+# Build the commit message
+commit_message="chore: bump version to $next_version"
+
+# Set the git user
+git config  user.name "Pavan Kumar Jadda"
+git config  user.email 17564080+pavankjadda@users.noreply.github.com
+
+# Make the git commit with the message
+git add .
+git commit -m "$commit_message"
+
+# Push the commit
+git push
