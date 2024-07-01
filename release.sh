4fa8e634ef202d4ff6e18d87e7bab0c229a5a660
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-07-01 17:56:20 -0400

    chore: updates docs

--- release.sh
+++ release.sh
@@ -5,9 +5,11 @@ next_version=$1
 
 # Path to the package.json file
 PACKAGE_JSON_FILE="projects/ng-kit/package.json"
+PACKAGE_JSON_FILE2="package.json"
 
 # Update the version in package.json
 jq --arg v "$next_version" '.version = $v' "$PACKAGE_JSON_FILE" > "tmp.json" && mv "tmp.json" "$PACKAGE_JSON_FILE"
+jq --arg v "$next_version" '.version = $v' "$PACKAGE_JSON_FILE2" > "tmp2.json" && mv "tmp2.json" "$PACKAGE_JSON_FILE2"
 
 # Build the commit message
 commit_message="chore: bump version to $next_version"
