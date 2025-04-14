5a68aa1882611179a23a699f554f3e4ce4c32a08
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-04-14 14:50:47 -0400

    feat: refactor autocomplete component to use reactive forms and ControlValueAccessor

--- projects/ng-kit/src/lib/components/autocomplete/autocomplete.component.ts
+++ projects/ng-kit/src/lib/components/autocomplete/autocomplete.component.ts
@@ -1,28 +1,29 @@
+import { AsyncPipe } from '@angular/common';
 import {
-  AfterContentChecked,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Input,
-  input,
-  OnChanges,
-  OnInit,
-  Optional,
-  output,
-  SimpleChanges,
-  ViewChild,
+	AfterContentChecked,
+	ChangeDetectorRef,
+	Component,
+	ElementRef,
+	forwardRef,
+	Input,
+	input,
+	OnChanges,
+	OnInit,
+	Optional,
+	output,
+	SimpleChanges,
+	ViewChild,
 } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { map, startWith } from 'rxjs/operators';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatOptionSelectionChange } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { TypeOfPipe } from '../../pipes/type-of.pipe';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { AsyncPipe } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { TypeOfPipe } from '../../pipes/type-of.pipe';
 
 /**
  * Reusable Auto Complete component that extends MatAutoComplete to show Clear icon and Arrow buttons
@@ -31,29 +32,36 @@ import { AsyncPipe } from '@angular/common';
  * @since 12.0.0
  */
 @Component({
-    selector: 'autocomplete, lib-autocomplete',
-    imports: [
-        ReactiveFormsModule,
-        TypeOfPipe,
-        MatFormFieldModule,
-        MatAutocompleteModule,
-        MatInputModule,
-        MatButtonModule,
-        MatIconModule,
-        AsyncPipe,
-    ],
-    templateUrl: './autocomplete.component.html'
+	selector: 'autocomplete, lib-autocomplete',
+	imports: [
+		ReactiveFormsModule,
+		TypeOfPipe,
+		MatFormFieldModule,
+		MatAutocompleteModule,
+		MatInputModule,
+		MatButtonModule,
+		MatIconModule,
+		AsyncPipe,
+	],
+	templateUrl: './autocomplete.component.html',
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => AutocompleteComponent),
+			multi: true,
+		},
+	],
 })
-export class AutocompleteComponent implements OnInit, OnChanges, AfterContentChecked {
+export class AutocompleteComponent implements OnInit, OnChanges, AfterContentChecked, ControlValueAccessor {
 	/**
 	 * Gets reference inputAutoComplete HTML attribute
 	 */
 	@ViewChild('inputAutoComplete') inputAutoComplete!: ElementRef;
 
 	/**
-	 * Input form group of the auto complete
+	 * Internal form control for the autocomplete
 	 */
-	@Input() inputFormGroup!: FormGroup;
+	control = new FormControl('');
 
 	/**
 	 * Label of the AutoComplete
@@ -96,15 +104,17 @@ export class AutocompleteComponent implements OnInit, OnChanges, AfterContentChe
 	required = input(false);
 
 	/**
+	 * Specifies if the autocomplete is disabled. Default is not required.
+	 */
+	disabled = input(false);
+
+	/**
 	 * List of Objects that need to be bind and searched for
 	 */
 	data = input<string[] | any[]>();
 
 	/**
 	 * Emit selected value on selection changes
-	 *
-	 * @author Pavan Kumar Jadda
-	 * @since 13.0.3
 	 */
 	onSelectionChange = output<any>();
 
@@ -114,24 +124,38 @@ export class AutocompleteComponent implements OnInit, OnChanges, AfterContentChe
 	arrowIconSubject = new BehaviorSubject('arrow_drop_down');
 
 	/**
-	 * Filtered options when user
+	 * Filtered options when user types
 	 */
 	filteredOptions: Observable<any[] | undefined> | undefined;
+	private value: any;
 
 	constructor(private cdRef: ChangeDetectorRef) {}
+
+	writeValue(value: any): void {
+		this.value = value;
+		this.control.setValue(value, { emitEvent: false });
+	}
+
+	registerOnChange(fn: any): void {
+		this.onChange = fn;
+		// Forward value changes from internal control to parent form
+		this.control.valueChanges.subscribe((value) => fn(value));
+	}
+
+	registerOnTouched(fn: any): void {
+		this.onTouched = fn;
+	}
+
+	setDisabledState(isDisabled: boolean): void {
+		isDisabled ? this.control.disable() : this.control.enable();
+	}
 
 	ngAfterContentChecked(): void {
 		this.cdRef.detectChanges();
 	}
 
-	/**
-	 * Define autocomplete search filter on search text changes
-	 *
-	 * @author Pavan Kumar Jadda
-	 * @since 12.0.0
-	 */
 	ngOnInit() {
-		this.filteredOptions = this.inputFormGroup?.get('autocomplete')?.valueChanges.pipe(
+		this.filteredOptions = this.control.valueChanges.pipe(
 			startWith(''),
 			map((value) => (typeof value === 'string' ? value : value !== null ? value[this.bindLabel()] : '')),
 			map(
@@ -149,36 +173,19 @@ export class AutocompleteComponent implements OnInit, OnChanges, AfterContentChe
 		this.displayFn = this.displayFn.bind(this);
 	}
 
-	/**
-	 * Clear input and Reset autocomplete form control
-	 *
-	 * @author Pavan Kumar Jadda
-	 * @since 12.0.0
-	 */
 	clearInput(evt: any): void {
 		evt.stopPropagation();
-		this.inputFormGroup.get('autocomplete')?.reset();
+		this.control.reset();
+		this.onChange(null);
 		this.inputAutoComplete?.nativeElement.focus();
 	}
 
-	/**
-	 * Open or Close panel
-	 *
-	 * @author Pavan Kumar Jadda
-	 * @since 12.0.0
-	 */
 	openOrClosePanel(evt: any, trigger: MatAutocompleteTrigger): void {
 		evt.stopPropagation();
 		if (trigger.panelOpen) trigger.closePanel();
 		else trigger.openPanel();
 	}
 
-	/**
-	 * Display function that is used to show the values
-	 *
-	 * @author Pavan Kumar Jadda
-	 * @since 12.0.0
-	 */
 	displayFn(object: any): string {
 		if (this.displayWith !== undefined && this.displayWith !== null && typeof this.displayWith === 'function') {
 			this.displayFn = this.displayWith.bind(this);
@@ -189,14 +196,14 @@ export class AutocompleteComponent implements OnInit, OnChanges, AfterContentChe
 		}
 	}
 
-	/**
-	 * Emit selected value
-	 * @param $event - Event emitted by autocomplete
-	 *
-	 * @author Pavan Kumar Jadda
-	 * @since 13.0.3
-	 */
 	emitSelectedValue($event: MatOptionSelectionChange) {
 		this.onSelectionChange.emit($event.source.value);
+		this.onChange($event.source.value);
+		this.onTouched();
 	}
+
+	// ControlValueAccessor implementation
+	private onChange: (value: any) => void = () => {};
+
+	private onTouched: () => void = () => {};
 }
