56bdcb509277227f3d8ca38b17287736807638f8
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-12-31 18:39:53 -0500

    docs: rename 'Edit BS Button' to 'Edit Bootstrap Button' for clarity and update build script name in package.json

--- projects/ng-kit-docs/src/app/categories/components/pages/buttons/edit-bs-button/ng-doc.page.ts
+++ projects/ng-kit-docs/src/app/categories/components/pages/buttons/edit-bs-button/ng-doc.page.ts
@@ -2,7 +2,7 @@ import { NgDocPage } from '@ng-doc/core';
 import ButtonsCategory from '../ng-doc.category';
 
 const EditBsButtonPage: NgDocPage = {
-	title: 'Edit BS Button',
+	title: 'Edit Bootstrap Button',
 	mdFile: './index.md',
 	category: ButtonsCategory,
 	order: 5,
