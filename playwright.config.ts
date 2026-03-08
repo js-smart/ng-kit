68bc6f254a094fe08505bfdbe5258db35be1d0df
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2026-03-08 01:17:33 -0500

    chore: bump ng-kit version to 21.4.0 and disable Firefox and WebKit tests
    - Updated package.json to reflect version 21.4.0.
    - Commented out Firefox and WebKit browser configurations in Playwright due to testing adjustments.

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,20 +51,20 @@ const config: PlaywrightTestConfig = {
 			},
 			dependencies: ['setup'],
 		},
-		{
-			name: 'firefox',
-			use: {
-				...devices['Desktop Firefox'],
-			},
-			dependencies: ['setup'],
-		},
-		{
-			name: 'webkit',
-			use: {
-				...devices['Desktop Safari'],
-			},
-			dependencies: ['setup'],
-		},
+		// {
+		// 	name: 'firefox',
+		// 	use: {
+		// 		...devices['Desktop Firefox'],
+		// 	},
+		// 	dependencies: ['setup'],
+		// },
+		// {
+		// 	name: 'webkit',
+		// 	use: {
+		// 		...devices['Desktop Safari'],
+		// 	},
+		// 	dependencies: ['setup'],
+		// },
 	],
 
 	/* Run your local dev server before starting the tests */
