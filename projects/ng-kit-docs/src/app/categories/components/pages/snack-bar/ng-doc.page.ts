f003eef68eb28608aff62013d61b8b3dc485a0d1
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2026-07-15 11:00:18 -0400

    feat(docs): add Open in StackBlitz button to Snack Bar page

--- projects/ng-kit-docs/src/app/categories/components/pages/snack-bar/ng-doc.page.ts
+++ projects/ng-kit-docs/src/app/categories/components/pages/snack-bar/ng-doc.page.ts
@@ -1,11 +1,13 @@
 import { NgDocPage } from '@ng-doc/core';
 import ComponentsCategory from '../../ng-doc.category';
+import { SnackBarOpenInStackblitzComponent } from './open-in-stackblitz.component';
 
 const SnackBar: NgDocPage = {
 	title: 'Snack Bar',
 	mdFile: './index.md',
 	order: 5,
 	category: ComponentsCategory,
+	demos: { SnackBarOpenInStackblitzComponent },
 };
 
 export default SnackBar;
